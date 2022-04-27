c279883d48f162652d1f4993f428a0f68e830b0b
Yaojl <903337655@qq.com>
2022-04-27 09:43:27 +0800

    开发master

--- src/main/java/com/qust/springboot/test.java
+++ src/main/java/com/qust/springboot/test.java
@@ -3,5 +3,6 @@ package com.qust.springboot;
 public class test {
     public static void main(String[] args) {
         System.out.println("创建Dev分支");
+        System.out.println("开发master");
     }
 }
